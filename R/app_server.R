0545bdd4de32bfff4ac289abde316201a2a8fbda
Sebastien Renaut <sebastien.renaut@gmail.com>
2022-11-28 12:01:22 -0500

    1st commit

---
+++ R/app_server.R
@@ -0,0 +1,61 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @import lubridate
+#' @import dplyr
+#' @import wesanderson
+#' @import plotly
+#' @import leaflet
+#' @noRd
+
+app_server <- function(input, output, session) {
+
+  parsed_bike_data = parse_bike_data(recalculate = T)
+
+  output$plotly_perstation = renderPlotly({
+    scatter_stats_plotly(data = parsed_bike_data,
+                         datelim=input$Dates_scat,
+                         station=input$stationID,
+                         add_trend=input$trend)
+    })
+
+  output$barplot_totals = renderPlotly({
+    barplotly_statistics(bike_data=parsed_bike_data[[3]],
+                       datelim=input$Dates_bar)
+  })
+
+  output$plotly_loess = renderPlotly({
+    loess_plotly(data = parsed_bike_data,
+                 stations=input$stationID_loess,
+                 datelim=input$Dates_loess,
+                 add_similar=input$similar,
+                 add_trend=input$trend_trend)
+  })
+
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$OpenStreetMap,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      addMarkers(data = cbind(parsed_bike_data[[3]]$Longitude,parsed_bike_data[[3]]$Latitude),
+                 label = lapply(parsed_bike_data[[3]]$labels,HTML))
+  })
+
+
+  output$general =
+    output$clinical <- renderUI({
+      para7 <- "J'utilise les données d'ici:données publiques disponibles sur la plateforme de <a href='https://www.donneesquebec.ca/recherche/dataset/vmtl-velos-comptage'> données québec</a>. Les données sont préparées et formattées localement
+      afin d'augmenter la rapidité de l'interface utilisateur.
+        <br/><br/>
+      sebastien.renaut@gmail.com --- 2022"
+      HTML(para7)
+    })
+
+
+
+
+}
+
+
---
+++ R/app_ui.R
@@ -0,0 +1,101 @@
+#' The application User-Interface
+#'
+#' @param request Internal parameter for `{shiny}`.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @import lubridate
+#' @import dplyr
+#' @import wesanderson
+#' @import plotly
+#' @import shinythemes
+#' @import leaflet
+#' @noRd
+
+app_ui = function(request){
+  tagList(
+    golem_add_external_resources(),
+    navbarPage(title = "Montréal - statistiques vélo",
+               theme = shinytheme("darkly"),
+
+               tabPanel("Localisation",
+                        icon = icon("map-pin"),
+                        mainPanel("",
+                                  width = 12,
+                                  leafletOutput("mymap",width = 1000, height=600))
+               ),
+               tabPanel("Vélo",
+                        icon = icon("bicycle"),
+                        sidebarPanel(width = 3,
+                          selectInput("stationID", "Station:",
+                                      choices = as.list(parse_bike_data()[[3]]$Nom), selected = parse_bike_data()[[3]]$Nom[4]),
+
+                           sliderInput("Dates_scat",
+                                    "Date:",
+                                    min = as.Date("2020-01-01","%Y-%m-%d"),
+                                    max = as.Date("2022-10-31","%Y-%m-%d"),
+                                    value=c(as.Date("2020-01-01","%Y-%m-%d"),as.Date("2022-10-31","%Y-%m-%d")),
+                                    timeFormat="%Y-%m-%d"),
+                          checkboxInput("trend","Moyenne", value = FALSE)
+                        )
+                        ,
+
+                        mainPanel(width = 9,"",
+                                  plotlyOutput("plotly_perstation")),
+               ),
+
+               tabPanel("Tendance",
+                        icon = icon("bicycle"),
+                        sidebarPanel(width = 3,
+                          selectInput("stationID_loess", "Station:",
+                                      choices = as.list(parse_bike_data()[[3]]$Nom), selected = parse_bike_data()[[3]]$Nom[4]),
+                        sliderInput("Dates_loess",
+                                    "Date:",
+                                    min = as.Date("2020-01-01","%Y-%m-%d"),
+                                    max = as.Date("2022-10-31","%Y-%m-%d"),
+                                    value=c(as.Date("2020-01-01","%Y-%m-%d"),as.Date("2022-10-31","%Y-%m-%d")),
+                                    timeFormat="%Y-%m-%d"),
+                        checkboxInput("trend_trend","Moyenne", value = FALSE),
+                        checkboxInput("similar","Rajouter des stations similaires", value = FALSE)),
+                        mainPanel(width = 9,"",plotlyOutput('plotly_loess')),
+               ),
+               tabPanel("Total",
+                        icon = icon("bars"),
+                        mainPanel("",width =12,plotlyOutput('barplot_totals')),
+               ),
+               tabPanel("Information",
+                        icon = icon("question"),
+                        mainPanel(width =12,strong("Description générale"),
+                                  htmlOutput("general"))
+               )
+               )
+               )
+}
+
+
+
+
+
+#' Add external Resources to the Application
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
+#' @import shiny
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @noRd
+golem_add_external_resources <- function() {
+  add_resource_path(
+    "www",
+    app_sys("app/www")
+  )
+
+  tags$head(
+    favicon(),
+    bundle_resources(
+      path = app_sys("app/www"),
+      app_title = "VeloMtl"
+    )
+    # Add here other external resources
+    # for example, you can add shinyalert::useShinyalert()
+  )
+}
