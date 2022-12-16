1a2bf60938608bb933f49f267c45e162eade014a
Sebastien Renaut <sebastien.renaut@gmail.com>
2022-12-16 11:08:54 -0500

    now passes as an R package

--- R/data_prep.R
+++ R/data_prep.R
@@ -11,13 +11,19 @@ library(leaflet)
 #' read data
 #' @return data
 #' @param recalculate logical. If you want to recalculate from the raw files..
-#' @param path Where is the data?
+#' @param path Where is the data stored?
 #' @examples
-#' eg = read_bike_data();
+#' data = read_bike_data()
 #' @export
-read_bike_data = function(path = "data/",recalculate = F){
-  if(file.exists(paste0(path,"comptage_summarised_2019_2022.csv")) == F | recalculate == T){
+read_bike_data = function(path = "VeloMtl/data/",recalculate = F){
+  if(file.exists(paste0(path,"comptage_summarised_2019_2022.rds")) == F | recalculate == T){
 
+    #get data 
+    system("wget https://data.montreal.ca/dataset/f170fecc-18db-44bc-b4fe-5b0b6d2c7297/resource/fd3da18e-8f87-44e4-890b-30dff05c12b8/download/comptage_velo_2022.csv -P data/raw/")
+    system("wget https://data.montreal.ca/dataset/f170fecc-18db-44bc-b4fe-5b0b6d2c7297/resource/b463fa29-8549-4664-ae68-b17ab604e0a5/download/comptage_velo_2021.csv -P data/raw/")
+    system("wget https://data.montreal.ca/dataset/f170fecc-18db-44bc-b4fe-5b0b6d2c7297/resource/eec17749-1a50-47b2-bc4e-1960ddc09eff/download/comptage_velo_2020.csv -P data/raw/")
+    system("wget https://data.montreal.ca/dataset/f170fecc-18db-44bc-b4fe-5b0b6d2c7297/resource/2cd0e082-f818-4014-9390-8e1c197ba806/download/comptage_velo_2019.csv -P data/raw/")
+        
     comptage_files = list.files(paste0(path,"raw/"),pattern="comptage_velo_20",full.names = T)
 
     comptage_list = lapply(as.list(comptage_files),read.csv)
@@ -49,15 +55,11 @@ read_bike_data = function(path = "data/",recalculate = F){
 
     comptage_summarised = comptage_complet %>% mutate(day_counts=floor_date(Date,unit= "day")) %>% group_by(day_counts) %>% summarise_at(colnames(comptage_complet)[-1],sum,na.rm=T)
 
-    write.csv(comptage_summarised,paste0(path,'comptage_summarised_2019_2022.csv'))
-
-    comptage_summarised = read.csv(paste0(path,"comptage_summarised_2019_2022.csv"),row.names = 1,check.names = F)
-    comptage_summarised$day_counts = ymd(comptage_summarised$day_counts)
+    saveRDS(comptage_summarised, file = paste0(path,'comptage_summarised_2019_2022.rds'))
   }
 
-  if(file.exists(paste0(path,"comptage_summarised_2019_2022.csv")) == T){
-    comptage_summarised = read.csv(paste0(path,"comptage_summarised_2019_2022.csv"),row.names = 1,check.names = F)
-    comptage_summarised$day_counts = ymd(comptage_summarised$day_counts)
+  if(file.exists(paste0(path,"comptage_summarised_2019_2022.rds")) == T){
+    comptage_summarised = readRDS(file = paste0(path,"comptage_summarised_2019_2022.rds"))  
   }
 
   comptage_summarised
@@ -69,15 +71,15 @@ read_bike_data = function(path = "data/",recalculate = F){
 #' parse bike data
 #' @return parsed bike data
 #' @param path Where is the data?
-#' @param recalculate logical. If you want to recalculate from the raw files.
 #' @examples
-#' eg = parse_bike_data();
+#' parsed_bike_data = parse_bike_data()
 #' @export
-parse_bike_data = function(path = "data/",recalculate = F){
-  if((file.exists(paste0(path,"comptage_summarised_plotly_2019_2022.csv")) == F) | recalculate == T) {
-    comptage_summarised = read_bike_data()
-    meta = read.csv(paste0(path,"localisation_des_compteurs_velo.csv"))
+parse_bike_data = function(path = "VeloMtl/data/"){
+    comptage_summarised = read_bike_data(path = path)
 
+    #if(file.exists(paste0(path,"localisation_des_compteurs_velo.csv") == F)) {meta = readRDS(file = paste0(path,"localisation_des_compteurs_velo.rds"))}
+    #if(file.exists(paste0(path,"localisation_des_compteurs_velo.csv"))) {meta = read.csv(paste0(path,"localisation_des_compteurs_velo.csv"))}
+    meta = readRDS(file = paste0(path,"localisation_des_compteurs_velo.rds"))    
     meta$Nom[3]  = 'Berri'
     meta$Nom[9]  = "Brébeuf / Rachel"
     meta$Nom[12] = "René-Lévesque / Wolfe"
@@ -96,11 +98,11 @@ parse_bike_data = function(path = "data/",recalculate = F){
     meta_match = meta[data_meta_match,]
 
     #add sums
-    meta_match$sum = apply(comptage_summarised[,-1],2,sum)
-    meta_match$sum2022 = apply(comptage_summarised[(comptage_summarised$day_counts >=as.Date("2022-01-01","%Y-%m-%d")) & (comptage_summarised$day_counts < as.Date("2023-01-01","%Y-%m-%d")) ,-1],2,sum)
-    meta_match$sum2021 = apply(comptage_summarised[(comptage_summarised$day_counts >=as.Date("2021-01-01","%Y-%m-%d")) & (comptage_summarised$day_counts < as.Date("2022-01-01","%Y-%m-%d")) ,-1],2,sum)
-    meta_match$sum2020 = apply(comptage_summarised[(comptage_summarised$day_counts >=as.Date("2020-01-01","%Y-%m-%d")) & (comptage_summarised$day_counts < as.Date("2021-01-01","%Y-%m-%d")) ,-1],2,sum)
-    meta_match$sum2019 = apply(comptage_summarised[(comptage_summarised$day_counts >=as.Date("2019-01-01","%Y-%m-%d")) & (comptage_summarised$day_counts < as.Date("2020-01-01","%Y-%m-%d")) ,-1],2,sum)
+    meta_match$sum = signif(apply(comptage_summarised[,-1],2,sum),4)
+    meta_match$sum2022 = signif(apply(comptage_summarised[(comptage_summarised$day_counts >=as.Date("2022-01-01","%Y-%m-%d")) & (comptage_summarised$day_counts < as.Date("2023-01-01","%Y-%m-%d")) ,-1],2,sum),4)
+    meta_match$sum2021 = signif(apply(comptage_summarised[(comptage_summarised$day_counts >=as.Date("2021-01-01","%Y-%m-%d")) & (comptage_summarised$day_counts < as.Date("2022-01-01","%Y-%m-%d")) ,-1],2,sum),4)
+    meta_match$sum2020 = signif(apply(comptage_summarised[(comptage_summarised$day_counts >=as.Date("2020-01-01","%Y-%m-%d")) & (comptage_summarised$day_counts < as.Date("2021-01-01","%Y-%m-%d")) ,-1],2,sum),4)
+    meta_match$sum2019 = signif(apply(comptage_summarised[(comptage_summarised$day_counts >=as.Date("2019-01-01","%Y-%m-%d")) & (comptage_summarised$day_counts < as.Date("2020-01-01","%Y-%m-%d")) ,-1],2,sum),4)
     meta_match = meta_match[order( meta_match$sum,decreasing=T),]
 
     #remove test stations
@@ -108,9 +110,8 @@ parse_bike_data = function(path = "data/",recalculate = F){
     
     #ordered count data
     temp = comptage_summarised[,-1]
-    data_meta_match = match(meta_match$ID,colnames(comptage_summarised)[-1])
+    data_meta_match = match(meta_match$ID,colnames(temp))
     comptage_summarised = data.frame(day_counts = comptage_summarised$day_counts,temp[,data_meta_match],check.names=F)
-
 
     #pretty labels for plots (map)
     meta_match$labels = paste0("<b>",
@@ -129,7 +130,6 @@ parse_bike_data = function(path = "data/",recalculate = F){
                                meta_match$ID
                                )
 
-
     #prepare data for plotly/ggplot
     comptage_summarised_plotly = data.frame(day_counts = rep(comptage_summarised$day_counts,ncol(comptage_summarised)-1))
     comptage_summarised_plotly$counts = unlist(c(comptage_summarised[,-1]))
@@ -138,7 +138,6 @@ parse_bike_data = function(path = "data/",recalculate = F){
 
     global_average= data.frame(day_counts=comptage_summarised[,1], counts = rowMeans(comptage_summarised[,-1]), station = 999, Nom = "Moyenne")
     comptage_summarised_plotly = rbind(comptage_summarised_plotly,global_average)
-
 
     #loess smoothing
     loess_smooth = list()
@@ -158,40 +157,32 @@ parse_bike_data = function(path = "data/",recalculate = F){
     }
 
     comptage_summarised_plotly$loess_smooth = unlist(loess_smooth)
-
     comptage_summarised_plotly$loess_smooth[comptage_summarised_plotly$loess_smooth<0] = 0
 
-    #write/read outputs
-    write.csv(comptage_summarised,paste0(path,'parsed_comptage_summarised_2019_2022.csv'))
-    write.csv(comptage_summarised_plotly,paste0(path,'comptage_summarised_plotly_2019_2022.csv'))
-    write.csv(meta_match,paste0(path,'parsed_localisation_des_compteurs_velo.csv'))
-
-    comptage_summarised = read.csv(paste0(path,"parsed_comptage_summarised_2019_2022.csv"),row.names = 1,check.names = F)
-    comptage_summarised$day_counts = ymd(comptage_summarised$day_counts)
-
-    meta = read.csv(paste0(path,"parsed_localisation_des_compteurs_velo.csv"))
-
-    comptage_summarised_plotly = read.csv(paste0(path,"comptage_summarised_plotly_2019_2022.csv"),row.names = 1,check.names = F)
-    comptage_summarised_plotly$day_counts = ymd(comptage_summarised_plotly$day_counts)
-    comptage_summarised_plotly$station = as.character(comptage_summarised_plotly$station)
-
-    parsed_bike_data = list(comptage_summarised_plotly,comptage_summarised,meta)
-  }
-
-  if(file.exists(paste0(path,"comptage_summarised_plotly_2019_2022.csv"))) {
-    comptage_summarised = read.csv(paste0(path,"parsed_comptage_summarised_2019_2022.csv"),row.names = 1,check.names = F)
-    comptage_summarised$day_counts = ymd(comptage_summarised$day_counts)
-
-    meta = read.csv(paste0(path,"parsed_localisation_des_compteurs_velo.csv"))
-
-    comptage_summarised_plotly = read.csv(paste0(path,"comptage_summarised_plotly_2019_2022.csv"),row.names = 1,check.names = F)
-    comptage_summarised_plotly$day_counts = ymd(comptage_summarised_plotly$day_counts)
-    comptage_summarised_plotly$station = as.character(comptage_summarised_plotly$station)
-
-    parsed_bike_data = list(comptage_summarised_plotly,comptage_summarised,meta)
-  }
-  parsed_bike_data
+    #final object
+    parsed_bike_data = list(comptage_summarised_plotly,comptage_summarised,meta_match)
+    parsed_bike_data
 }
 
+
+
+#' dummy data
+#'
+#' dummy data
+#' @return a list
+#' @examples
+#' data_test = dummy_data()
+#' @export
+dummy_data = function(){
+  list(
+  data.frame(day_counts=c(rep(as.Date("2020-01-01","%Y-%m-%d"),10),rep(as.Date("2021-01-01","%Y-%m-%d"),10)),
+             station=rep(c(1001,1e+08),10),
+             counts = seq(1,20),
+             loess_smooth =seq(1,20),
+             Nom=rep(c('Boyer / Everett','moyenne'),10)),
+  data.frame(Nom=letters[1:10],sum=sample(1:10)),
+  data.frame(Nom=letters[1:10],sum=sample(1:10),ID = seq(1001,1010))
+)
+}
 
 
--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -1,18 +1,9 @@
 
 
 #fake test data
-data_test = list(
-  data.frame(day_counts=c(rep(as.Date("2020-01-01","%Y-%m-%d"),10),rep(as.Date("2021-01-01","%Y-%m-%d"),10)),
-             station=rep(c(1001,1e+08),10),
-             counts = seq(1,20),
-             loess_smooth =seq(1,20),
-             Nom=rep(c('a','moyenne'),10)),
-  data.frame(Nom=letters[1:10],sum=sample(1:10)),
-  data.frame(Nom=letters[1:10],sum=sample(1:10),ID = seq(1001,1010))
-  )
 
 app_VeloMtl = run_app()
-
+data_test = dummy_data()
 
 #
 test_that("dummy true", {
@@ -35,17 +26,17 @@ test_that("default_colors(default=F) works", {
 
 
 test_that("barplotly_statistics works", {
-  expect_true(all(class(barplotly_statistics(bike_data = data_test[[3]]))==c("plotly","htmlwidget")))
+  expect_true(all(class(barplotly_statistics(data = data_test))==c("plotly","htmlwidget")))
 })
 
 
 test_that("loess_plotly works", {
-  expect_true(all(class(loess_plotly(data = data_test,stations="a"))==c("plotly","htmlwidget")))
+  expect_true(all(class(loess_plotly(data = data_test,stations="Boyer / Everett"))==c("plotly","htmlwidget")))
 })
 
 
 test_that("scatter_stats_plotly works", {
-  expect_true(all(class(scatter_stats_plotly(data = data_test,stations="a"))==c("plotly","htmlwidget")))
+  expect_true(all(class(scatter_stats_plotly(data = data_test,stations="Boyer / Everett"))==c("plotly","htmlwidget")))
 })
 
 
